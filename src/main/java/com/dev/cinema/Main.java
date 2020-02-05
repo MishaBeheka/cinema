c67bd02a1f9b717cc2b03982e89d8f666543fdea
Misha <mishabeheka@gmail.com>
2020-02-05 12:59:20 +0200

    Done entity CinemaHall

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -1,7 +1,9 @@
 package com.dev.cinema;
 
 import com.dev.cinema.lib.Injector;
+import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
+import com.dev.cinema.service.MovieCinemaHallService;
 import com.dev.cinema.service.MovieService;
 
 public class Main {
@@ -9,14 +11,20 @@ public class Main {
 
     public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-
-        movieService.getAll().forEach(System.out::println);
-
         Movie movie = new Movie();
         movie.setTitle("Star Wars");
-        System.out.println("Before MovieService.add");
+        movie.setDescription("Fantastic");
         movie = movieService.add(movie);
         System.out.println("Before movieService.getAll()");
         movieService.getAll().forEach(System.out::println);
+
+        MovieCinemaHallService cinemaHallService =
+                (MovieCinemaHallService) injector.getInstance(MovieCinemaHallService.class);
+
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(100);
+        cinemaHall.setDescription("IMAX");
+        cinemaHall = cinemaHallService.add(cinemaHall);
+        cinemaHallService.getAll().forEach(System.out::println);
     }
 }
