cbfcbeaa03a24176674c032f9f8aad1ab1841732
MishaBeheka <46327654+MishaBeheka@users.noreply.github.com>
2020-02-11 20:35:41 +0200

    check travis (#7)
    * check travis
    
    * check travis

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -18,10 +18,14 @@ import com.dev.cinema.service.ShoppingCartService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+
+
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
