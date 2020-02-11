0c44edda47b92d90291b3d96fb771b1c167c5577
Misha <mishabeheka@gmail.com>
2020-02-11 20:42:07 +0200

    check travis

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -19,6 +19,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Arrays;
+
+
+
+
+
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
