ca30aee9f5bbb5bf9037990100c0be092257904c
Misha <mishabeheka@gmail.com>
2020-02-12 14:45:30 +0200

    fix_travis 3

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -18,6 +18,8 @@ import com.dev.cinema.service.ShoppingCartService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+
+
 import java.util.Arrays;
 import java.util.List;
 
