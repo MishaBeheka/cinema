d8a03502b554409407d9ca1797ffb27f4553c5a7
Misha <mishabeheka@gmail.com>
2020-02-19 16:50:29 +0200

    Fix mistake in db.properties and complete MovieController

--- src/main/java/com/dev/cinema/controllers/MovieController.java
+++ src/main/java/com/dev/cinema/controllers/MovieController.java
@@ -1,41 +1,49 @@
 package com.dev.cinema.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.dev.cinema.dto.MovieRequestDto;
 import com.dev.cinema.dto.MovieResponseDto;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
-    @Autowired
-    private MovieService movieService;
+    private final MovieService movieService;
 
-    @PostMapping("/movies")
-    public void create(MovieRequestDto movieDto) {
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping(value = "/add")
+    public void create(@RequestBody MovieRequestDto movieDto) {
         Movie movie = new Movie();
         movie.setTitle(movieDto.getTitle());
         movie.setDescription(movieDto.getDescription());
         movieService.add(movie);
     }
 
-    @GetMapping("/movies")
+    @GetMapping(value = "/all")
     public List<MovieResponseDto> getAllMovies() {
-        List<MovieResponseDto> allMovies = new ArrayList<>();
-        for (Movie movie : movieService.getAll()) {
-            MovieResponseDto movieDto = new MovieResponseDto();
-            movieDto.setTitle(movie.getTitle());
-            movieDto.setDescription(movie.getDescription());
-            allMovies.add(movieDto);
-        }
-        return allMovies;
+        return movieService.getAll()
+                .stream()
+                .map(this::migrateToMovieDto)
+                .collect(Collectors.toList());
+    }
+
+    private MovieResponseDto migrateToMovieDto(Movie movie) {
+        MovieResponseDto movieDto = new MovieResponseDto();
+        movieDto.setTitle(movie.getTitle());
+        movieDto.setDescription(movie.getDescription());
+        return movieDto;
     }
 }
