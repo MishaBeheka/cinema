d68e39ccf34c72918c40dc171a8d455ec7b817ee
Misha <mishabeheka@gmail.com>
2020-02-19 19:22:20 +0200

    Started to do MovieSessionController

---
+++ src/main/java/com/dev/cinema/controllers/MovieSessionController.java
@@ -0,0 +1,30 @@
+package com.dev.cinema.controllers;
+
+import com.dev.cinema.dto.MovieSessionRequestDto;
+import com.dev.cinema.service.MovieCinemaHallService;
+import com.dev.cinema.service.MovieService;
+import com.dev.cinema.service.MovieSessionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie-sessions")
+public class MovieSessionController {
+
+    private final MovieService movieService;
+    private final MovieCinemaHallService cinemaHallService;
+    private final MovieSessionService movieSessionService;
+
+    public MovieSessionController(MovieService movieService, MovieCinemaHallService cinemaHallService, MovieSessionService movieSessionService) {
+        this.movieService = movieService;
+        this.cinemaHallService = cinemaHallService;
+        this.movieSessionService = movieSessionService;
+    }
+
+    @PostMapping(value = "/add")
+    public void create(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+
+    }
+}
