29b60c6d259559b6dabecabf6c3c3153fd1adbce
Misha <mishabeheka@gmail.com>
2020-02-19 19:39:31 +0200

    Added methods getById() in MovieService and MovieCinemaHallService

--- src/main/java/com/dev/cinema/controllers/MovieSessionController.java
+++ src/main/java/com/dev/cinema/controllers/MovieSessionController.java
@@ -1,6 +1,7 @@
 package com.dev.cinema.controllers;
 
 import com.dev.cinema.dto.MovieSessionRequestDto;
+import com.dev.cinema.model.MovieSession;
 import com.dev.cinema.service.MovieCinemaHallService;
 import com.dev.cinema.service.MovieService;
 import com.dev.cinema.service.MovieSessionService;
@@ -26,5 +27,10 @@ public class MovieSessionController {
     @PostMapping(value = "/add")
     public void create(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
 
+    }
+
+    private MovieSession buildMovieSession(MovieSessionRequestDto movieSessionRequestDto) {
+        MovieSession movieSession = new MovieSession();
+        ;
     }
 }
